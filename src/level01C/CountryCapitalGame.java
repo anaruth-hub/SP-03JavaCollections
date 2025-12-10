f60c23f870a36f8a3a99b323ea8e8db93ba72181
Ana Ruth <aniarinna@gmail.com>
2025-12-11 00:22:45 +0100

    Load countries from txt file, generate 10 random questions, validate answers, show score and save results to classificacio.txt. Fixed formatting and file path issues.

--- src/level01C/CountryCapitalGame.java
+++ src/level01C/CountryCapitalGame.java
@@ -1,4 +1,100 @@
 package level01C;
 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
 public class CountryCapitalGame {
-}
+
+    private final Map<String, String> countryCapitalMap = new HashMap<>();
+    private final int questionsCount;
+
+    public CountryCapitalGame(int questionsCount) {
+        this.questionsCount = questionsCount;
+    }
+
+
+    public void loadCountries(String fileName) {
+        Path filePath = Paths.get(fileName);
+
+        if (!Files.exists(filePath)) {
+            System.out.println("Error: file " + fileName + "not found");
+            return;
+
+        }
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath.toFile()))) {
+            String line;
+
+            while ((line = reader.readLine()) != null)  {
+                String trimmedLine = line.trim();
+                if (trimmedLine.isEmpty()) {
+                    continue;
+
+                }
+
+                String[] parts = trimmedLine.split("\\s+");
+                if (parts.length != 2) {
+                    continue;
+
+                }
+
+                String country = parts[0].trim().replace('_', ' ');
+                String capital = parts[1].trim().replace('_', ' ');
+
+                if (!country.isEmpty() && !capital.isEmpty()) {
+                    countryCapitalMap.put(country, capital);
+
+
+                }
+
+            }
+
+            System.out.println("Countries loaded: " + countryCapitalMap.size());
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + fileName);
+        }
+    }
+
+    public int play(Scanner scanner) {
+    int score = 0;
+
+        List<String> countries = new ArrayList<>(countryCapitalMap.keySet());
+
+        if (countries.size() < questionsCount){
+            System.out.println("countries are missing from the load");
+            return 0;
+        }
+
+        Collections.shuffle(countries);
+        List<String> selected = countries.subList(0, questionsCount);
+
+        for (int i = 0; i < selected.size(); i++) {
+            String country = selected.get(i);
+            String correctCapital = countryCapitalMap.get(country);
+
+            System.out.println("Question " + (i + 1) + ": Please tell me, what is the capital of " + country + "?");
+            System.out.println("What is your answer?: ");
+            String participantAnswer = scanner.nextLine().trim();
+
+            if (participantAnswer.equalsIgnoreCase(correctCapital)) {
+                score++;
+                System.out.println("Correct answer!");
+            } else {
+                System.out.println("Incorrect answer" + correctCapital);
+            }
+
+            System.out.println();
+
+        }
+
+        return score;
+
+        }
+    }
+
--- src/level01C/CountryCapitalGameMain.java
+++ src/level01C/CountryCapitalGameMain.java
@@ -1,4 +1,56 @@
 package level01C;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class CountryCapitalGameMain {
-}
+    private static final String COUNTRIES_FILE = "countries.txt";
+    private static final  String SCORES_FILE = "classificacio.txt";
+    private static final  int QUESTIONS = 10;
+
+    public static void main(String[] args) {
+        CountryCapitalGame game = new CountryCapitalGame(QUESTIONS);
+        game.loadCountries(COUNTRIES_FILE);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please, enter your name: ");
+        String name = scanner.nextLine().trim();
+
+        int score = game.play(scanner);
+
+        System.out.println("Player: " + name);
+        System.out.println("Score: " + "/" + QUESTIONS);
+
+        saveScore(name, score);
+
+        scanner.close();
+
+
+    }
+
+    private static void saveScore(String name, int score) {
+        Path file = Paths.get(SCORES_FILE);
+
+        try {
+            if (!Files.exists(file)) {
+                Files.createFile(file);
+
+            }
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(file.toFile(), true))) {
+                writer.write(name + " - " + score + "/" + QUESTIONS);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error writing score file: " + SCORES_FILE);
+
+            }
+        }
+    }
+
