972130ff6563691c82bec2f3ef3b34db319324f9
Ana Ruth <aniarinna@gmail.com>
2025-12-11 01:43:25 +0100

    feat: implement restaurant de duplication and custom sorting

--- src/level02A/MainRestaurant.java
+++ src/level02A/MainRestaurant.java
@@ -1,4 +1,43 @@
 package level02A;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class MainRestaurant {
+
+    public static void main(String[] args) {
+
+        Set<Restaurant> restaurantSet = new HashSet<>();
+    Restaurant rest1 = new Restaurant("Restaurant A", 9);
+    Restaurant rest2 = new Restaurant("Restaurant A", 7);
+    Restaurant rest3 = new Restaurant("Restaurant B", 8);
+    Restaurant rest5 = new Restaurant("Restaurant C", 10);
+
+    restaurantSet.add(rest1);
+    restaurantSet.add(rest2);
+    restaurantSet.add(rest3);
+    restaurantSet.add(rest5);
+
+        System.out.println("Restaurants in HashSet (no exact duplicates): ");
+        for (Restaurant restaurant : restaurantSet) {
+            System.out.println(restaurant);
+
+        }
+
+
+        List<Restaurant> sortedRestaurants = new ArrayList<>(restaurantSet);
+
+        Collections.sort(sortedRestaurants);
+
+            System.out.println();
+            System.out.println("Restaurants sorted by name and qualification");
+        for (Restaurant restaurant : sortedRestaurants) {
+            System.out.println("name: " + restaurant.getName()
+                    + ", rating: " + restaurant.getQualification());
+        }
+
+    }
 }
--- src/level02A/Restaurant.java
+++ src/level02A/Restaurant.java
@@ -1,4 +1,59 @@
 package level02A;
 
-public class Restaurant {
+import java.util.Objects;
+
+public class Restaurant implements  Comparable <Restaurant> {
+
+    private final String name;
+    private final int qualification;
+
+    public Restaurant(String name, int qualification) {
+        this.name = name;
+        this.qualification = qualification;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQualification() {
+        return qualification;
+    }
+
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+
+        }
+
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+
+        Restaurant other = (Restaurant) object;
+
+        return qualification == other.qualification &&
+                Objects.equals(name, other.name);
+    }
+
+    public int hashCode() {
+        return Objects.hash(name, qualification);
+    }
+
+    @Override
+    public int compareTo(Restaurant other) {
+        int nameComparison = this.name.compareToIgnoreCase(other.name);
+        if (nameComparison != 0) {
+            return nameComparison;
+        }
+
+        return Integer.compare(other.qualification, this.qualification);
+
+    }
+
+    public String toString() {
+        return "Restaurant{name='" + name + "', qualification=" + qualification + "}";
+
+    }
 }
